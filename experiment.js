29a9d662ffa441dc8ccabc783042b19ab640825e
Neo <neogoldahlgren@gmail.com>
2025-05-13 11:54:58 +0200

    Update experiment.js

--- experiment.js
+++ experiment.js
@@ -1,2 +1,14 @@
+import * as klara from "./klara.js";
+import * as neo from "./neo.js";
+import * as sigge from "./sigge.js";
+
 let usersInput = prompt("Write something!");
-console.log(usersInput)
+if (usersInput === "neo") {
+    neo.hello();
+} else if (usersInput === "klara") {
+    klara.hello();
+} else if (usersInput === "sigge") {
+    sigge.hello();
+} else {
+    console.log(usersInput)
+}
