fb6b5e5b0d5f563e05a20a87af412f646877724d
klaraassvensson <klara.svensson04@gmail.com>
2025-05-14 13:18:14 +0200

    allow cors
    added headers that allows cors

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
 import { serveFile, serveDir } from "jsr:@std/http";
+
+function handler(){
+    const headersCORS = new Headers();
+
+    headersCORS.set("Access-Control-Allow-Origin", "*");
+    headersCORS.set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+    headersCORS.set("Access-Control-Allow-Headers", "Content-Type");
+    headersCORS.set("Content-Type", "application/json" );
+    
+}
