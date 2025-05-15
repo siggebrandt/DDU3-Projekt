76da7f22823dbc0bf235e4ed05cc99b78b3977fc
Sigge Brandt <sigge.brandt567@gmail.com>
2025-05-15 11:25:32 +0200

    flyttade addeventlisteners till endast relevant

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,8 +1,2 @@
 const websiteURL = window.location.origin; // använd dena variabel för websidans-URL, representerar localhost OCH serverlänken
 
-document.getElementById("createGameButton").addEventListener("click", function() { 
-    window.location.href = `${websiteURL}/create`
-});
-document.getElementById("joinGameButton").addEventListener("click", function() { 
-    window.location.href = `${websiteURL}/join`
-});
