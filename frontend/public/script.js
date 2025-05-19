7fd4ae1f4954c4b731d94e92524f42f1ece8beda
Sigge Brandt <sigge.brandt567@gmail.com>
2025-05-19 10:39:18 +0200

    Update script.js

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,2 +1,2 @@
 const websiteURL = window.location.origin; // använd dena variabel för websidans-URL, representerar localhost OCH serverlänken
-
+const pathname = window.location.pathname // (t.ex. "/quiz")
